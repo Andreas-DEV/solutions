6ce19613330a9e87eeaa5accd26597ac4bde5e67
Andreas G. Christensen <48524202+Andreas-DEV@users.noreply.github.com>
2024-04-26 09:16:25 +0200

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
     import Header from "$lib/Header.svelte";
     import Footer from "$lib/Footer.svelte";
     import Hero from "$lib/Hero.svelte";
-    import Why from "$lib/Why.svelte";
+
     import Accordion from "$lib/Accordion.svelte";
 </script>
 
